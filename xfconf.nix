3d3690c0369288779626477d19a466a62aaf5f3f
jordan <sudo.sendmail.jordan@gmail.com>
2025-08-12 18:14:21 -0700

    sync

--- xfconf.nix
+++ xfconf.nix
@@ -1,10 +1,12 @@
 {
 xfconf.settings = {
   xfce4-keyboard-shortcuts = {
-       "xfwm4/custom/<Super>Up" = "maximize_window_key";
-       "xfwm4/custom/<Super>Down" = "tile_down_key";
-       "xfwm4/custom/<Super>Right" = "tile_right_key";
-       "xfwm4/custom/<Super>Left" = "tile_left_key";
+    "xfwm4/custom/<Super>Up" = "maximize_window_key";
+    "xfwm4/custom/<Super>Down" = "tile_down_key";
+    "xfwm4/custom/<Super>Right" = "tile_right_key";
+    "xfwm4/custom/<Super>Left" = "tile_left_key";
+    # "backdrop/screen0/monitor0/workspace0/last-image" = "/etc/nixos/plants.jpg";
+  
   };
   xfce4-terminal = {
     "misc-menubar-default" = false;
