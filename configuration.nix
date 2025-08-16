47364fd37b6ae20d38b1e5b4f9e36bf65ad4ce6c
jordan <sudo.sendmail.jordan@gmail.com>
2025-08-16 05:22:07 -0700

    sync

--- configuration.nix
+++ configuration.nix
@@ -87,56 +87,56 @@ home.packages = with pkgs; [
     rofi
     fastfetch
 
-    # vscode
-    (vscode-with-extensions.override {
-    vscodeExtensions = with vscode-extensions; [
-        bbenoist.nix
-        golang.go
-        bbenoist.nix
-        redhat.vscode-xml
-        redhat.vscode-yaml
-        usernamehw.errorlens
-        ibm.output-colorizer
-        ritwickdey.liveserver
-        esbenp.prettier-vscode
-        astro-build.astro-vscode
-        mechatroner.rainbow-csv
-        gitlab.gitlab-workflow
-        catppuccin.catppuccin-vsc
-        github.github-vscode-theme
-        yzhang.markdown-all-in-one
-        elixir-lsp.vscode-elixir-ls
-        vscode-icons-team.vscode-icons
-        ms-vscode.cpptools-extension-pack 
-        vscjava.vscode-java-pack # intelliCode included
-        # streetsidesoftware.code-spell-checker
-    ] ++ 
-    pkgs.vscode-utils.extensionsFromVscodeMarketplace [ # nix-prefetch-url 
-    { # https://tal7aouy.gallery.vsassets.io/_apis/public/gallery/publisher/tal7aouy/extension/theme/3.1.0/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage
-        name = "theme";
-        publisher = "tal7aouy";
-        version = "3.1.0";
-        sha256 = "1p06l2d30h3c12jcls533hw61irf4nmlxakg5xvxbmlzj55nm4ic";
-    }
-    { # https://rangav.gallery.vsassets.io/_apis/public/gallery/publisher/rangav/extension/vscode-thunder-client/2.37.5/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage
-        name = "vscode-thunder-client";
-        publisher = "rangav";
-        version = "2.37.5";
-        sha256 = "1dignnk5p1j6xs2nkvy37c5ighydifvisg5lz9h63w30i0mj779z";
-    }
-    { # https://qwtel.gallery.vsassets.io/_apis/public/gallery/publisher/qwtel/extension/sqlite-viewer/0.10.6/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage
-        name = "sqlite-viewer";
-        publisher = "qwtel";
-        version = "0.10.6";
-        sha256 = "13d5g0jksxpachvs2d05bvzaaa54wpmcaik4kzcs15acamdjxpvl";
-    }
-    { # https://rw-smups.gallery.vsassets.io/_apis/public/gallery/publisher/rw-smups/extension/gruberfruity/0.8.0/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage
-        name = "gruberfruity";
-        publisher = "rw-smups";
-        version = "0.8.0";
-        sha256 = "1grf69n270gsizg2z053ldjn3k0sy8iv8ja8xrv5iv183x993j1v";
-    }
-    ];})
+    vscode
+    # (vscode-with-extensions.override {
+    # vscodeExtensions = with vscode-extensions; [
+    #     bbenoist.nix
+    #     golang.go
+    #     bbenoist.nix
+    #     redhat.vscode-xml
+    #     redhat.vscode-yaml
+    #     usernamehw.errorlens
+    #     ibm.output-colorizer
+    #     ritwickdey.liveserver
+    #     esbenp.prettier-vscode
+    #     astro-build.astro-vscode
+    #     mechatroner.rainbow-csv
+    #     gitlab.gitlab-workflow
+    #     catppuccin.catppuccin-vsc
+    #     github.github-vscode-theme
+    #     yzhang.markdown-all-in-one
+    #     elixir-lsp.vscode-elixir-ls
+    #     vscode-icons-team.vscode-icons
+    #     ms-vscode.cpptools-extension-pack 
+    #     vscjava.vscode-java-pack # intelliCode included
+    #     # streetsidesoftware.code-spell-checker
+    # ] ++ 
+    # pkgs.vscode-utils.extensionsFromVscodeMarketplace [ # nix-prefetch-url 
+    # { # https://tal7aouy.gallery.vsassets.io/_apis/public/gallery/publisher/tal7aouy/extension/theme/3.1.0/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage
+    #     name = "theme";
+    #     publisher = "tal7aouy";
+    #     version = "3.1.0";
+    #     sha256 = "1p06l2d30h3c12jcls533hw61irf4nmlxakg5xvxbmlzj55nm4ic";
+    # }
+    # { # https://rangav.gallery.vsassets.io/_apis/public/gallery/publisher/rangav/extension/vscode-thunder-client/2.37.5/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage
+    #     name = "vscode-thunder-client";
+    #     publisher = "rangav";
+    #     version = "2.37.5";
+    #     sha256 = "1dignnk5p1j6xs2nkvy37c5ighydifvisg5lz9h63w30i0mj779z";
+    # }
+    # { # https://qwtel.gallery.vsassets.io/_apis/public/gallery/publisher/qwtel/extension/sqlite-viewer/0.10.6/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage
+    #     name = "sqlite-viewer";
+    #     publisher = "qwtel";
+    #     version = "0.10.6";
+    #     sha256 = "13d5g0jksxpachvs2d05bvzaaa54wpmcaik4kzcs15acamdjxpvl";
+    # }
+    # { # https://rw-smups.gallery.vsassets.io/_apis/public/gallery/publisher/rw-smups/extension/gruberfruity/0.8.0/assetbyname/Microsoft.VisualStudio.Services.VSIXPackage
+    #     name = "gruberfruity";
+    #     publisher = "rw-smups";
+    #     version = "0.8.0";
+    #     sha256 = "1grf69n270gsizg2z053ldjn3k0sy8iv8ja8xrv5iv183x993j1v";
+    # }
+    # ];})
 ]; home.stateVersion = "25.05"; };
 
 # system
@@ -153,6 +153,8 @@ environment.systemPackages = with pkgs; [
     gh
     git
     github-desktop
+
+    nodejs_22
     
     discord
     librewolf
