42f209fca7a1030fd6239e846c28e1c47f9adc21
jordan <sudo.sendmail.jordan@gmail.com>
2025-08-18 05:49:14 -0700

    sync

--- modules/xfce.nix
+++ modules/xfce.nix
@@ -24,7 +24,7 @@ displayManager.lightdm.enable = true;
 xkb.layout = "us";
 xkb.variant = "";
 
-exportConfiguration = true;
+# exportConfiguration = true;
 excludePackages = with pkgs; [
 # xterm
 ];
@@ -58,57 +58,11 @@ home-manager.users.${specialArgs.user} = { config, ... }: ({
     # size = 64;
     # x11.enable = true;
 # };
+
+# default applications - helpers
 # home.file.".config/xfce4/helpers.rc".text = "TerminalEmulator=alacritty";
+
 home.file = {
-".config/xfce4/desktop/accels.scm".text = ''
-; xfdesktop GtkAccelMap rc-file         -*- scheme -*-
-; this file is an automated accelerator map dump
-;
-; (gtk_accel_path "<Actions>/XfceDesktop/primary-menu-2" "Menu")
-; (gtk_accel_path "<Actions>/XfdesktopFileIconManager/open-with-other" "")
-; (gtk_accel_path "<Actions>/XfdesktopIconView/activate-3" "Return")
-; (gtk_accel_path "<Actions>/XfdesktopFileIconManager/properties-3" "<Alt>KP_Enter")
-; (gtk_accel_path "<Actions>/XfdesktopFileIconManager/trash" "Delete")
-; (gtk_accel_path "<Actions>/XfceDesktop/next-background" "")
-; (gtk_accel_path "<Actions>/XfdesktopFileIconManager/cut-2" "")
-; (gtk_accel_path "<Actions>/XfdesktopFileIconManager/cut" "<Primary>x")
-; (gtk_accel_path "<Actions>/XfdesktopIconView/unselect-all" "Escape")
-; (gtk_accel_path "<Actions>/XfdesktopFileIconManager/copy-2" "<Primary>Insert")
-; (gtk_accel_path "<Actions>/XfdesktopIconView/activate" "space")
-; (gtk_accel_path "<Actions>/XfdesktopIconView/activate-2" "KP_Space")
-; (gtk_accel_path "<Actions>/XfdesktopFileIconManager/open" "<Primary>o")
-; (gtk_accel_path "<Actions>/XfdesktopIconView/select-all" "<Primary>a")
-; (gtk_accel_path "<Actions>/XfdesktopFileIconManager/properties-2" "<Alt>ISO_Enter")
-; (gtk_accel_path "<Actions>/XfdesktopFileIconManager/empty-trash" "")
-; (gtk_accel_path "<Actions>/XfdesktopFileIconManager/open-trash" "")
-; (gtk_accel_path "<Actions>/XfdesktopFileIconManager/trash-3" "")
-; (gtk_accel_path "<Actions>/XfdesktopFileIconManager/properties" "<Alt>Return")
-; (gtk_accel_path "<Actions>/XfdesktopFileIconManager/copy" "<Primary>c")
-; (gtk_accel_path "<Actions>/XfceDesktop/reload-2" "Reload")
-; (gtk_accel_path "<Actions>/XfdesktopFileIconManager/delete-3" "")
-; (gtk_accel_path "<Actions>/XfdesktopFileIconManager/trash-2" "KP_Delete")
-; (gtk_accel_path "<Actions>/XfceDesktop/reload" "<Primary>r")
-; (gtk_accel_path "<Actions>/XfdesktopFileIconManager/paste-into-folder" "")
-; (gtk_accel_path "<Actions>/XfdesktopFileIconManager/paste-2" "<Shift>Insert")
-; (gtk_accel_path "<Actions>/XfceDesktop/secondary-menu" "<Primary><Shift>F10")
-; (gtk_accel_path "<Actions>/XfdesktopFileIconManager/delete-2" "<Shift>KP_Delete")
-; (gtk_accel_path "<Actions>/XfdesktopIconView/toggle-cursor-2" "<Primary>KP_Space")
-; (gtk_accel_path "<Actions>/XfceDesktop/reload-1" "F5")
-; (gtk_accel_path "<Actions>/XfdesktopFileIconManager/open-filesystem" "")
-; (gtk_accel_path "<Actions>/XfdesktopIconView/activate-5" "KP_Enter") 
-; (gtk_accel_path "<Actions>/XfceDesktop/primary-menu" "<Shift>F10")
-; (gtk_accel_path "<Actions>/XfdesktopFileIconManager/create-document" "")
-; (gtk_accel_path "<Actions>/XfdesktopFileIconManager/create-folder" "<Primary><Shift>n")
-; (gtk_accel_path "<Actions>/XfdesktopIconView/toggle-cursor" "<Primary>space")
-; (gtk_accel_path "<Actions>/XfdesktopFileIconManager/toggle-show-hidden" "<Primary>h")
-; (gtk_accel_path "<Actions>/XfdesktopFileIconManager/open-home" "<Alt>Home")
-; (gtk_accel_path "<Actions>/XfdesktopIconView/activate-4" "ISO_Enter")
-; (gtk_accel_path "<Actions>/XfdesktopFileIconManager/delete" "<Shift>Delete")
-; (gtk_accel_path "<Actions>/XfdesktopFileIconManager/paste" "<Primary>v")
-; (gtk_accel_path "<Actions>/XfdesktopFileIconManager/rename" "F2")
-; (gtk_accel_path "<Actions>/XfdesktopIconView/arrange-icons" "")
-; (gtk_accel_path "<Actions>/XfceDesktop/secondary-menu-2" "<Primary>Menu")
-'';
 
 ".config/xfce4/terminal/accels.scm".text = ''
 ; xfce4-terminal GtkAccelMap rc-file         -*- scheme -*-
@@ -269,13 +223,16 @@ xfconf.settings.xfce4-desktop = {
 };
 
 xfconf.settings.xfce4-keyboard-shortcuts = {
+"xfwm4/custom/override" = true;
 "xfwm4/custom/<Super>Up" = "maximize_window_key";
-"xfwm4/custom/<Super>Down" = "tile_down_key";
-"xfwm4/custom/<Super>Right" = "tile_right_key";
 "xfwm4/custom/<Super>Left" = "tile_left_key";
+"xfwm4/custom/<Super>Right" = "tile_right_key";
+"xfwm4/custom/<Super>Down" = "tile_down_key";
 "xfwm4/custom/<Primary>q" = "close_window_key";
+
 "commands/custom/<Primary>g" = "minimize_window_key";
 "commands/custom/<Primary>space" = "rofi -show drun";
+"commands/custom/<Shift>space" = "xfce4-terminal";
 };
 
 xfconf.settings.xfce4-panel = {
